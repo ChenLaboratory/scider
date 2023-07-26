def3e7189c0f07589f1434b04897d13e61ec0325
Yunshun Chen <yuchen@wehi.edu.au>
2023-07-26 11:53:24 +1000

    Update compute_density().

--- R/compute_density.R
+++ R/compute_density.R
@@ -1,71 +1,63 @@
 #' Perform kernel density estimation on SpatialExperiment
 #'
 #' @param spe A SpatialExperiment object.
-#' @param mode Choose either points or pixels. Specifying whether to compute the
-#' density at a grid pixel location or at at point.
-#' @param grid_size Grid size to use when the mode is set to pixels.
 #' @param bandwidth The smoothing bandwidth. By default performing automatic bandwidth
-#' selection using cross-validation using function spatstat.explore::bw.diggle.
 #' @param weights Optional weights to be attached to the points.
 #' @param kernel The smoothing kernel.Options are gaussian, epanechnikov, quartic or disc.
+#' @param scale Scaling factor for density.
+#' @param ngrid_x Number of grids in the x-direction.
+#' @param ngrid_y Number of grids in the y-direction.
+#' @param grid_length_x Grid length in the x-direction.
+#' @param grid_length_y Grid length in the y-direction.
+#' @param xlim The range of the x-coordinates of the image.
+#' @param ylim The range of the y-coordinates of the image.
+#' 
+#' selection using cross-validation using function spatstat.explore::bw.diggle.
 #'
 #' @return A SpatialExperiment object.
 #' @export
 #'
 #' @examples
-#' 
-#' data("xenium_bc_spe")
-#' 
-#' compute_density(spe)
-#' 
-compute_density <- function(spe, mode = c("points","pixels"), bandwidth = NULL, weights = NULL,
-                            kernel = c("gaussian", "epanechnikov", "quartic", "disc"),
-                            grid_size = 100, xlim = NULL, ylim = NULL){
-
+compute_density <- function(spe, bandwidth = NULL, weights = NULL,
+                            kernel = c("gaussian", "epanechnikov", "quartic", "disc"), scale=1e4,
+                            ngrid_x = 100, ngrid_y = NULL, 
+                            grid_length_x = NULL, grid_length_y = NULL, xlim, ylim
+){
+  
   if(length(kernel) == 4){
     kernel <- "gaussian"
   }
-
+  
   sc <- SpatialExperiment::spatialCoords(spe)
   
-  if(is.null(xlim)){
-    xlim <- c(min(sc[,1]), max(sc[,1]))
-  }
-  
-  if(is.null(ylim)){
-    ylim <- c(min(sc[,2]), max(sc[,2]))
-  }
-
   y <- spatstat.geom::ppp(sc[,1], sc[,2], xlim, ylim)
-
+  
   if(is.null(bandwidth)){
     bandwidth <- spatstat.explore::bw.diggle(y)
   }
-  
-  if(length(mode) == 2){
-    mode <- "pixels"
+
+  if(!is.null(grid_length_x) | !is.null(grid_length_y)){
+    if(is.null(grid_length_y)) 
+      grid_length_y <- grid_length_x
+    if(is.null(grid_length_x)) 
+      grid_length_x <- grid_length_y
+
+    ngrid_x <- round(diff(xlim) / grid_length_x)
+    ngrid_y <- round(diff(ylim) / grid_length_y)
+  } else {
+    if(is.null(ngrid_y))
+      ngrid_y <- round(diff(ylim) / diff(xlim) * ngrid_x)
   }
 
   density_est <- spatstat.explore::density.ppp(y, sigma = bandwidth,
                                                kernel = kernel, weights = weights,
-                                               at = mode, dimyx = c(grid_size, grid_size))
+                                               at = "pixels", dimyx = c(ngrid_y, ngrid_x))
 
-  if(mode == "points"){
-    colData(spe)$density = density_est
+  grid_density <- density_est$v * scale
+  rownames(grid_density) <- density_est$yrow
+  colnames(grid_density) <- density_est$xcol
+  grid_density <- setNames(reshape2::melt(grid_density), c('y_grid', 'x_grid', 'density'))
 
-  } else if (mode == "pixels"){
-    grid_density <- density_est$v
-    rownames(grid_density) <- density_est$yrow
-    colnames(grid_density) <- density_est$xcol
-    grid_density <- grid_density |> 
-      as.data.frame() |> 
-      rownames_to_column("y_grid") |> 
-      pivot_longer(-y_grid, names_to = "x_grid", values_to = "density") |> 
-      dplyr::select(c("x_grid","y_grid","density"))
-
-    metadata(spe) <- list("grid_density" = grid_density)
-  }
-
-  return(spe)
+  #grid_density[,c(2,1,3)]
+  return(list(grid_density = grid_density[,c(2,1,3)], density_est = density_est))
 }
-
