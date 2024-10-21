46c840df4e15327defddc9e37cefdb46744dda87
Yunshun Chen <yuchen@wehi.edu.au>
2024-10-22 10:49:36 +1100

    scider v1.3.10 - gridSPE() has an option to calculate grid-level count matrix for each cell type

--- R/gridSPE.R
+++ R/gridSPE.R
@@ -4,7 +4,11 @@
 #' @param cell.count Logical. Whether to obtain the number of cells within 
 #' each group identified by the 'id' column in colData(spe). Default to FALSE.
 #' @param id A character. The name of the column of colData(spe) containing
-#' the cell type identifiers. Set to cell_type by default.
+#' the cell type identifiers. Set to 'cell_type' by default.
+#' @param split.count.by A character. The name of the column of colData(spe). 
+#' When it is not NULL, a grid-level count matrix is calculated for each member 
+#' specified in that column of colData(spe) and stored in the assays(spe).
+#' Set to 'cell_type' by default. 
 #' 
 #' @return A SpatialExperiment object.
 #' @export
@@ -17,14 +21,14 @@
 #'
 #' spe_grid <- gridSPE(spe)
 #'
-gridSPE <- function(spe, cell.count = FALSE, id = 'cell_type') {
+gridSPE <- function(spe, cell.count = FALSE, id = 'cell_type', split.count.by = id) {
     if (!("grid_density" %in% names(spe@metadata))) {
         stop("Please run gridDensity before using this function.")
     }
 
     grid_data <- spe@metadata$grid_density[,(1:2)]
 
-    grids = sf::st_sfc(grid2sf(spe))
+    grids <- sf::st_sfc(grid2sf(spe))
     # Assign cells to grids
     xy_allcells <- sf::st_as_sf(
       as.data.frame(SpatialExperiment::spatialCoords(spe)), 
@@ -35,7 +39,18 @@ gridSPE <- function(spe, cell.count = FALSE, id = 'cell_type') {
 
     # Obtain gene counts at the grid level
     assays <- list()
-    assays$counts <- as.matrix(SummarizedExperiment::assay(spe,"counts") %*% overlap_ind)
+    if(is.null(split.count.by)){
+        assays$counts <- as.matrix(SummarizedExperiment::assay(spe,"counts") %*% overlap_ind)
+    } else {
+        split_names <- names(table(spe@colData[[split.count.by]]))
+        split_names_clean <- janitor::make_clean_names(split_names)
+        for(i in 1:length(split_names)){
+            sub <- spe@colData[[split.count.by]] == split_names[i]
+            assays[[i+1]] <- as.matrix(SummarizedExperiment::assay(spe,"counts")[,sub,drop=FALSE] %*% overlap_ind[sub,,drop=FALSE])
+        }
+        assays[[1]] <- Reduce(`+`, assays[-1])
+        names(assays) <- c("counts", paste0("counts_", split_names_clean))
+    }
 
     # Obtain cell type counts at the grid level
     if(cell.count){
@@ -57,8 +72,8 @@ gridSPE <- function(spe, cell.count = FALSE, id = 'cell_type') {
                                                     rowData = SummarizedExperiment::rowData(spe),
                                                     spatialCoordsNames = c("x_grid", "y_grid"))
     
-    spe_out@metadata = spe@metadata
-    spe_out@metadata$grid_info$gridLevelAnalysis = TRUE
+    spe_out@metadata <- spe@metadata
+    spe_out@metadata$grid_info$gridLevelAnalysis <- TRUE
 
     return(spe_out)
 }
